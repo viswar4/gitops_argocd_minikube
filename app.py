1f7f8e84c204935bc81963f895088e53200293cb
Raghav <raghav@gmail.com>
2023-05-08 07:23:55 +0530

    Final Commit

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def health():
 
 @app.route("/")
 def home():
-    return "Welcome to Jenkins advanced CICD pipeline"
+    return "Welcome to Jenkins advanced CICD pipeline with gitops"
    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
